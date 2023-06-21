17c2865cc6bc6a22a1f36e0f33f7893d5c6db765
Sos <sosmanucharyan89@gmail.com>
2023-06-21 18:47:06 +0400

    easy carousel

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: "@/plugins/carousel3d.js", mode: 'client'}
+    { src: "@/plugins/carousel3d.js", mode: 'client'},
+    { src: "@/plugins/vueEasySlider.js", ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
