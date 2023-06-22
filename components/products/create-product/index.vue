ec15a5cac989a31c557ae94bf14c5951ffebd1ab
Sos <sosmanucharyan89@gmail.com>
2023-06-22 17:46:55 +0400

    create product component slots

---
+++ components/products/create-product/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+    <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+    </div>
+    <div class="grid gap-4 mb-8 sm:grid-cols-2">
+      <div>
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+        <slot name="name"></slot>
+      </div>
+      <div>
+        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Category
+        </label>
+        <slot name="select_category"></slot>
+      </div>
+      <div>
+        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+        <slot name="price"></slot>
+      </div>
+      <div>
+        <label for="stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
+        <slot name="stock"></slot>
+      </div>
+      <div class="sm:col-span-2">
+        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Description
+        </label>
+        <slot name="description"></slot>
+      </div>
+    </div>
+    <div class="mb-4 space-y-4 sm:flex sm:space-y-0">
+      <div class="flex items-center mr-4">
+        <input id="inline-checkbox" type="checkbox" value="" name="sellingType" class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">In-store only</label>
+      </div>
+      <div class="flex items-center mr-4">
+        <input id="inline-2-checkbox" type="checkbox" value="" name="sellingType" class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-2-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Online selling only</label>
+      </div>
+      <div class="flex items-center mr-4">
+        <input checked="" id="inline-checked-checkbox" type="checkbox" value="" name="sellingType" class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-checked-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Both in-store and online</label>
+      </div>
+    </div>
+    <div class="mb-4">
+      <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Images</span>
+      <div class="flex justify-center items-center">
+        <label for="dropzone-file" class="flex md:mr-6 flex-col justify-center items-center w-1/2 h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+          <div class="flex flex-col justify-center items-center pt-5 pb-6">
+            <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span class="font-semibold">Click to upload</span>
+              or drag and drop
+            </p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+          </div>
+          <slot name="image-input"></slot>
+        </label>
+        <div class="w-1/2 flex flex-col items-center justify-between">
+          <img class="w-full h-64 rounded-xl object-cover object-center " :src="imageUrl" alt="Selected Image">
+          <slot name="img_err"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+      <button
+        type="button"
+        class="w-full sm:w-auto justify-center text-white inline-flex bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        @click="$emit('create')"
+      >
+        Add product
+      </button>
+
+      <nuxt-link to="/admin/products" class="w-full justify-center sm:w-auto text-gray-500 inline-flex items-center bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+          <svg class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+          </svg>
+          Discard
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  layout: 'AdminLayout',
+  middleware: 'admin',
+  props: {
+    imageUrl: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
