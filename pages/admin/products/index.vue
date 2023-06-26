bada82a4a9f6a31b0ce31673bb9d7e1799e30e08
Sos89 <sosmanucharyan89@gmail.com>
2023-06-27 03:46:19 +0400

    pagination

--- pages/admin/products/index.vue
+++ pages/admin/products/index.vue
@@ -363,16 +363,14 @@
               <span class="font-semibold text-gray-900 dark:text-white">{{ getTotal }}</span>
               Entries
             </span>
-            <div class="inline-flex mt-2 xs:mt-0">
-              <!-- Buttons -->
-              <button @click="prevPage" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
-                Prev
-              </button>
-              <button @click="nextPage" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                Next
-                <svg aria-hidden="true" class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-              </button>
+            <div class="inline-flex mt-2 xs:mt-0 ml-auto">
+
+              <div v-for="link of getLinks">
+                <button @click="handleLinkClick(link)" :class="[link.active ? 'active' : '']" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span v-html="link.label"></span>
+                </button>
+              </div>
+
             </div>
           </div>
 
@@ -506,7 +504,8 @@ export default {
       'getLastPage',
       'getPerPage',
       'getTotal',
-      'getCurrentPage'
+      'getCurrentPage',
+       'getLinks'
     ])
   },
   methods :{
@@ -561,20 +560,23 @@ export default {
         }
       })
     },
-    prevPage(){
-      if (this.page > 1){
-        this.page --
-        this.fetchProducts(this.page)
+    async handleLinkClick(link) {
+      try {
+        const url = new URL(link.url);
+        const page = url.searchParams.get('page');
+        await this.fetchProducts(page);
+      } catch (error) {
+        console.error(error);
+        const defaultPage = 1; // Set your default page value here
+        await this.fetchProducts(defaultPage);
       }
     },
-    nextPage(){
-      this.page ++
-      this.fetchProducts(this.page)
-    }
   }
 }
 </script>
 
 <style scoped>
-
+ .active{
+   background: darkgray;
+ }
 </style>
--- store/products.js
+++ store/products.js
@@ -9,6 +9,7 @@ export const state = () => ({
   lastPage: 1,
   perPage: 5,
   total: 0,
+  links: []
 })
 
 export const getters = {
@@ -39,18 +40,22 @@ export const getters = {
   getTotal(state){
     return state.total
   },
+  getLinks(state){
+    return state.links
+  }
 }
 
 export const actions = {
   async fetchProducts({ commit }, page ) {
     try {
       const { data } = await this.$axios.get(`/api/products?page=${page}`);
-      console.log(data)
+      console.log(data.productData)
       commit('setProducts', data.productData.data);
       commit('setCurrentPage', data.productData.current_page);
       commit('setLastPage', data.productData.last_page);
       commit('setPerPage', data.productData.per_page);
       commit('setTotal', data.productData.total);
+      commit('setLinks', data.productData.links);
       return true;
     } catch (err) {
       commit('setErrorMessages', err.response.data.errors)
@@ -154,4 +159,7 @@ export const mutations = {
   setTotal(state, total) {
     state.total = total;
   },
+  setLinks(state, links){
+    state.links = links
+  }
 }
